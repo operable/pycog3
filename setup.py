6fdefcc0e62849b06175ffd1f8b580948f7f2605
Kevin Smith <kevin@operable.io>
2016-06-09 11:47:36 -0400

    Wrestling w/distutils

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup (
     name = "pycog3",
-    version = "0.1.25",
+    version = "0.1.26",
     scripts = ["bin/cog-command"],
     description = "Command library for the Cog ChatOps platform for Python3",
     author = "Kevin Smith",
     author_email = "kevin@operable.io",
     url = "https://github.com/cog-bundles/pycog3",
     download_url = "https://github.com/cog-bundles/pycog3/tarball/0.1.25",
-    packages = ["pycog3"],
+    packages = ["cog"],
     requires = ["requests (>=2.10)", "PyYAML (>=3.11)"],
     keywords = ["bot", "devops", "chatops", "automation"],
     classifiers = [
