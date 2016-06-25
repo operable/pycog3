95df367768a11a6f4d3707d146b7b32e6b7a054b
Kevin Smith <kevin@operable.io>
2016-06-25 14:10:55 -0400

    Bumped version number

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup (
     name = "pycog3",
-    version = "0.1.26",
+    version = "0.2.0",
     scripts = ["bin/cog-command"],
     description = "Command library for the Cog ChatOps platform for Python3",
     author = "Kevin Smith",
     author_email = "kevin@operable.io",
     url = "https://github.com/cog-bundles/pycog3",
-    download_url = "https://github.com/cog-bundles/pycog3/tarball/0.1.25",
+    download_url = "https://github.com/cog-bundles/pycog3/tarball/0.2.0",
     packages = ["cog"],
     requires = ["requests (>=2.10)", "PyYAML (>=3.11)"],
     keywords = ["bot", "devops", "chatops", "automation"],
